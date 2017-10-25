ec1759afc2f1c3fad014bb270fba90fda73c191f
ciaran1344 <ciaran1344@hotmail.com>
2017-10-25 17:33:00 +0100

    Added constructor and AddEdge methods

--- src/DAG.java
+++ src/DAG.java
@@ -1,24 +1,54 @@
+import java.util.ArrayList;
+
 public class DAG {
+    private final int vertices;    //Number of vertices in DAG.
+    private final ArrayList<Integer>[] adjacencyList;
 
     /**
      * Constructor.
+     *
      * @param v - Number of vertices.
      */
-    public DAG (int v) {
-        //TODO
+    public DAG(int v) {
+        if (v > 0) {
+            vertices = v;
+        }
+        else {
+            vertices = 0;
+        }
+
+        //Create list of adjacent vertices for each vertex.
+        adjacencyList = (ArrayList<Integer>[]) new ArrayList[vertices];
+        for (int i = 0; i < vertices; i++) {
+            adjacencyList[i] = new ArrayList<Integer>();
+        }
     }
 
     /**
      * Add a directed edge from v -> w.
+     *
      * @param v - Origin vertex.
      * @param w - Destination vertex.
      */
     public void addEdge(int v, int w) {
-        //TODO
+        ArrayList<Integer> current = adjacencyList[v];
+
+        if (!current.contains(w)) {
+            current.add(w);
+
+            if (containsCycle()) {
+                current.remove(w);
+                System.out.println("Cycle-completing edge " + v + "->" + w + " ignored.");
+            }
+        }
+        else {
+            System.out.println("Duplicate edge " + v + "->" + w + " ignored.");
+        }
     }
 
     /**
      * Return the vertices pointing from v.
+     *
      * @param v - The origin vertex.
      * @return The destination vertices, if any.
      */
@@ -36,6 +66,14 @@ public class DAG {
     }
 
     /**
+     * @return Whether the current graph contains a cycle.
+     */
+    public boolean containsCycle() {
+        //TODO
+        return false;
+    }
+
+    /**
      * @return The number of edges in the graph.
      */
     public int edgeCount() {
@@ -44,8 +82,18 @@ public class DAG {
     }
 
     public String toString() {
-        //TODO
-        return null;
-    }
+        String string = "";
 
+        for (int i = 0; i < vertices; i++) {
+            ArrayList<Integer> current = adjacencyList[i];
+            string += i + ": ";
+
+            for (int j = 0; j < current.size(); j++) {
+                string += current.get(j) + " ";
+            }
+            string += "\n";
+        }
+
+        return string;
+    }
 }
