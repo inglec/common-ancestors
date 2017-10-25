1cac69b266a26b8363fe4881c49532b011bc08d1
ciaran1344 <ciaran1344@hotmail.com>
2017-10-25 15:05:39 +0100

    Added methods to be implemented for a directed graph

---
+++ src/DAG.java
@@ -0,0 +1,51 @@
+public class DAG {
+
+    /**
+     * Constructor.
+     * @param v - Number of vertices.
+     */
+    public DAG (int v) {
+        //TODO
+    }
+
+    /**
+     * Add a directed edge from v -> w.
+     * @param v - Origin vertex.
+     * @param w - Destination vertex.
+     */
+    public void addEdge(int v, int w) {
+        //TODO
+    }
+
+    /**
+     * Return the vertices pointing from v.
+     * @param v - The origin vertex.
+     * @return The destination vertices, if any.
+     */
+    public Iterable<Integer> adjacent(int v) {
+        //TODO
+        return null;
+    }
+
+    /**
+     * @return The number of vertices in the graph.
+     */
+    public int vertexCount() {
+        //TODO
+        return -1;
+    }
+
+    /**
+     * @return The number of edges in the graph.
+     */
+    public int edgeCount() {
+        //TODO
+        return -1;
+    }
+
+    public String toString() {
+        //TODO
+        return null;
+    }
+
+}
